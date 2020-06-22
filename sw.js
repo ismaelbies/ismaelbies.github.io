c556e7297c78c28daf9e9a25078e51025066ca96
ismaelbies <57952913+ismaelbies@users.noreply.github.com>
2020-06-22 16:40:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ var filesToCache = [
 self.addEventListener('install', function(e) {
                       
                       e.waitUntil(
-                        caches.open(cacheName).then(function(cache)) {
+                        caches.open(cacheName).then(function(cache) {
                             return cache.addAll(filesToCache);
                         })
                       );
