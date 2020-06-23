1f2493e1b7a8d4aa86810772766f3467c93324eb
ismaelbies <57952913+ismaelbies@users.noreply.github.com>
2020-06-23 12:25:53 -0300

    Add files via upload

---
+++ acesso.js
@@ -0,0 +1,15 @@
+function getList() {
+	alert("acess");
+	vat xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange= function() {
+		if(this.readyState==4 && this.status!=200) {
+			alert("Erro");
+		} if(this.readyState==4 && this.status==200) {
+			var content = this.responseText;
+			var value = JSON.parse(content);
+		}
+	}
+	xhttp.open("GET","https://viacep.com.br/ws/01001000/json/",true);
+	xhttp.send();
+	
+}
