0c5dd57c03b5e904850667d0b0c4a49ac8e89f16
ismaelbies <57952913+ismaelbies@users.noreply.github.com>
2020-06-23 13:03:28 -0300

    Update acesso.js

--- acesso.js
+++ acesso.js
@@ -8,14 +8,19 @@ function getList() {
 		} if(this.readyState==4 && this.status==200) {
 			var content = this.responseText;
 			var value = JSON.parse(content);
+			
+			alert(value);
+			
 			texto = Object.values(value);
 			
+			alert(texto);
+			
+			var p = document.getElementById("cep");
+			p.innerHTML = texto;
 		}
 	}
 	xhttp.open("GET","https://viacep.com.br/ws/01001000/json/",true);
 	xhttp.send();
 	
-	var p = document.getElementById("cep");
-	p.innerHTML = texto;
 }
 getList();
